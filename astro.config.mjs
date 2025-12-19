e5562933d87609df0660e2a0bdebdcf5d955a839
ovixdev <ovix365@gmail.com>
2025-12-19 22:09:34 +0100

    chore: update dependencies and add global styles
    - Added new dependencies for MDX, sitemap, Tailwind CSS typography, and Vite.
    - Created a new global CSS file with Tailwind CSS imports and custom theme configuration.
    - Defined base styles and custom utility classes for buttons and containers.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ovixdev.github.io',
+  base: '/acme-blog-pro',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx(), sitemap()],
+});
